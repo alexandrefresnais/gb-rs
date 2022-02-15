c690b48b5213e5b3a2e23217d91eb7f90f86e291
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2022-02-15 21:04:45 +0100

    add: first cpu and mmu approach

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 
-mod cpu
-{
+mod cpu {
     struct Registers {
         a: u8,
         b: u8,
@@ -12,10 +11,25 @@ mod cpu
         h: u8,
         l: u8,
         pc: u16,
-        sp: u16
+        sp: u16,
     }
 
     impl Registers {
+        fn new() -> Registers {
+            Registers {
+                a: 0,
+                b: 0,
+                c: 0,
+                d: 0,
+                e: 0,
+                f: 0,
+                h: 0,
+                l: 0,
+                pc: 0,
+                sp: 0,
+            }
+        }
+
         fn af(&self) -> u16 {
             ((self.a as u16) << 8) | self.f as u16
         }
@@ -32,10 +46,53 @@ mod cpu
             ((self.h as u16) << 8) | self.l as u16
         }
     }
+
+    pub struct CPU {
+        reg: Registers,
+    }
+
+    impl CPU {
+        pub fn new() -> CPU {
+            CPU {
+                reg: Registers::new(),
+            }
+        }
+
+        pub fn run_cycle(&mut self, mmu: super::mmu::MMU) -> u8 {
+            // In this implementation, the CPU will give the number of cycle
+            // to run on other components
+            let opcode = mmu.readb(self.reg.pc);
+            self.reg.pc += 1;
+            4
+        }
+    }
+}
+
+mod mmu {
+    pub struct MMU {
+        rom: Vec<u8>,
+    }
+
+    impl MMU {
+        pub fn new(rom: Vec<u8>) -> MMU {
+            MMU { rom: rom }
+        }
+
+        pub fn readb(&self, addr: u16) -> u8 {
+            if addr < 0x8000 {
+                *self.rom.get(addr as usize).unwrap()
+            } else {
+                0
+            }
+        }
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rom = std::fs::read(&args[1]);
+    let mut mmu = mmu::MMU::new(rom.unwrap());
+    let mut cpu = cpu::CPU::new();
+    cpu.run_cycle(mmu);
     println!("Hello, world!");
 }
