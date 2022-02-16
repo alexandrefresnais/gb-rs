2178b5eec49e0deebe429583264986ecbfd42f03
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2022-02-16 23:53:09 +0100

    update: split cpu, registers and mmu into multiple files

---
+++ src/mmu.rs
@@ -0,0 +1,57 @@
+pub struct MMU {
+    rom: Vec<u8>,
+    memory: [u8; 0x10000]
+}
+
+impl MMU {
+    pub fn new(rom: Vec<u8>) -> MMU {
+        MMU { rom: rom, memory:  [0; 0x10000]}
+    }
+
+    pub fn readb(&self, addr: u16) -> u8 {
+        if addr < 0x8000 {
+            *self.rom.get(addr as usize).unwrap()
+        } else {
+            self.memory[addr as usize]
+        }
+    }
+
+    pub fn readw(&self, addr: u16) -> u16 {
+        let lsb = if addr < 0x8000 {
+            *self.rom.get(addr as usize).unwrap()
+        } else {
+            self.memory[addr as usize]
+        };
+
+        let msb = if addr < 0x8000 {
+            *self.rom.get((addr + 1) as usize).unwrap()
+        } else {
+            self.memory[(addr + 1) as usize]
+        };
+
+        super::to_u16(msb, lsb)
+    }
+
+    pub fn writeb(&mut self, addr: u16, value: u8) {
+        if addr < 0x8000 {
+            self.rom[addr as usize] = value;
+        }
+        else
+        {
+            self.memory[addr as usize] = value;
+        }
+    }
+
+    pub fn writew(&mut self, addr: u16, value: u16) {
+        let (msb, lsb) = super::to_u8(value);
+
+        if addr < 0x8000 {
+            self.rom[addr as usize] = lsb as u8;
+            self.rom[(addr + 1) as usize] = msb as u8;
+        }
+        else {
+            self.memory[addr as usize] = lsb as u8;
+            self.memory[(addr + 1) as usize] = msb as u8;
+        }
+    }
+}
---
+++ src/registers.rs
@@ -0,0 +1,113 @@
+pub struct Registers {
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
+    pub pc: u16,
+    pub sp: u16,
+}
+
+impl Registers {
+    pub fn new() -> Registers {
+        Registers {
+            a: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: 0,
+            h: 0,
+            l: 0,
+            pc: 0x100,
+            sp: 0,
+        }
+    }
+
+    pub fn af(&self) -> u16 {
+        ((self.a as u16) << 8) | self.f as u16
+    }
+
+    pub fn bc(&self) -> u16 {
+        ((self.b as u16) << 8) | self.c as u16
+    }
+
+    pub fn de(&self) -> u16 {
+        ((self.d as u16) << 8) | self.e as u16
+    }
+
+    pub fn hl(&self) -> u16 {
+        ((self.h as u16) << 8) | self.l as u16
+    }
+
+    pub fn set_af(&mut self, value: u16) {
+        self.a = ((value & 0xff00) >> 8) as u8;
+        self.f = (value & 0xff) as u8;
+    }
+
+    pub fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xff00) >> 8) as u8;
+        self.c = (value & 0xff) as u8;
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        self.d = ((value & 0xff00) >> 8) as u8;
+        self.e = (value & 0xff) as u8;
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        self.h = ((value & 0xff00) >> 8) as u8;
+        self.l = (value & 0xff) as u8;
+    }
+
+    pub fn set_z(&mut self, value: bool) {
+        if value {
+            self.f = self.f | 0b1000000;
+        } else {
+            self.f = self.f & 0b0111111;
+        }
+    }
+
+    pub fn set_n(&mut self, value: bool) {
+        if value {
+            self.f = self.f | 0b100000;
+        } else {
+            self.f = self.f & 0b011111;
+        }
+    }
+
+    pub fn set_h(&mut self, value: bool) {
+        if value {
+            self.f = self.f | 0b10000;
+        } else {
+            self.f = self.f & 0b01111;
+        }
+    }
+
+    pub fn set_c(&mut self, value: bool) {
+        if value {
+            self.f = self.f | 0b1000;
+        } else {
+            self.f = self.f & 0b0111;
+        }
+    }
+
+    pub fn get_z(&mut self) -> bool {
+        (self.f & 0b1000000) == 0b1000000
+    }
+
+    pub fn get_n(&mut self) -> bool {
+        (self.f & 0b100000) == 0b100000
+    }
+
+    pub fn get_h(&mut self) -> bool {
+        (self.f & 0b10000) == 0b10000
+    }
+
+    pub fn get_c(&mut self) -> bool {
+        (self.f & 0b1000) == 0b1000
+    }
+}
