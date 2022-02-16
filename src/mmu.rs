e818547772ac8a1080b8a32cde14cc4954907a65
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2022-02-17 00:42:54 +0100

    add: mmu initialization

--- src/mmu.rs
+++ src/mmu.rs
@@ -7,7 +7,40 @@ use super::lcd;
 
 impl MMU {
     pub fn new(rom: Vec<u8>) -> MMU {
-        MMU { rom: rom, memory:  [0; 0x10000]}
+        let mut mmu = MMU { rom: rom, memory:  [0; 0x10000]};
+        mmu.memory[0xFF05] = 0x00;
+        mmu.memory[0xFF06] = 0x00;
+        mmu.memory[0xFF07] = 0x00;
+        mmu.memory[0xFF10] = 0x80;
+        mmu.memory[0xFF11] = 0xBF;
+        mmu.memory[0xFF12] = 0xF3;
+        mmu.memory[0xFF14] = 0xBF;
+        mmu.memory[0xFF16] = 0x3F;
+        mmu.memory[0xFF17] = 0x00;
+        mmu.memory[0xFF19] = 0xBF;
+        mmu.memory[0xFF1A] = 0x7F;
+        mmu.memory[0xFF1B] = 0xFF;
+        mmu.memory[0xFF1C] = 0x9F;
+        mmu.memory[0xFF1E] = 0xBF;
+        mmu.memory[0xFF20] = 0xFF;
+        mmu.memory[0xFF21] = 0x00;
+        mmu.memory[0xFF22] = 0x00;
+        mmu.memory[0xFF23] = 0xBF;
+        mmu.memory[0xFF24] = 0x77;
+        mmu.memory[0xFF25] = 0xF3;
+        mmu.memory[0xFF26] = 0xF1;
+        mmu.memory[0xFF40] = 0x91;
+        mmu.memory[0xFF42] = 0x00;
+        mmu.memory[0xFF43] = 0x00;
+        mmu.memory[0xFF45] = 0x00;
+        mmu.memory[0xFF47] = 0xFC;
+        mmu.memory[0xFF48] = 0xFF;
+        mmu.memory[0xFF49] = 0xFF;
+        mmu.memory[0xFF4A] = 0x00;
+        mmu.memory[0xFF4B] = 0x00;
+        mmu.memory[0xFFFF] = 0x00;
+
+        mmu
     }
 
     pub fn readb(&self, addr: u16) -> u8 {
