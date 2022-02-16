cebabc0d0e77c0b88816cd4f19dace27a575bf9e
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2022-02-17 00:37:48 +0100

    update: running by frame

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,36 @@ fn to_u16(msb: u8, lsb: u8) -> u16 {
 mod cpu;
 mod mmu;
 mod registers;
+mod lcd;
+
+use cpu::CPU;
+use mmu::MMU;
+use lcd::LCD;
+
+fn run_one_frame(cpu: &mut CPU, mmu: &mut MMU) {
+    // GameBoy can execute 4194304 cycles per second
+    // We want 60 frames per second
+    // So we run 69905 each frame
+    const FRAME_CYLES: u64 = 69905;
+
+    let mut cycles: u64 = 0;
+    while cycles < FRAME_CYLES {
+        let cpu_cycles = cpu.run_cycle(mmu);
+
+        cycles += cpu_cycles as u64;
+    }
+
+    // Render the screen
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rom = std::fs::read(&args[1]);
     let mut mmu = mmu::MMU::new(rom.unwrap());
     let mut cpu = cpu::CPU::new();
+    let mut lcd = lcd::LCD::new();
 
     loop {
-        cpu.run_cycle(&mut mmu);
+        run_one_frame(&mut cpu, &mut mmu);
     }
 }
