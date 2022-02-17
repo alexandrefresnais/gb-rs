f0f3577fd0ac84e3bd68fdeaf3cb98bdfc7e7f58
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2022-02-17 16:38:58 +0100

    add: is_lcd_enabled

--- src/lcd.rs
+++ src/lcd.rs
@@ -1,6 +1,7 @@
 // Memory address storing the current scanline
 pub const SCANLINE_REGISTER: usize = 0xFF44;
 const LCD_STATUS_REGISTER: usize = 0xFF41;
+const LCD_CONTROL_REGISTER: u16 = 0xFF40;
 
 // Number of cpu clock cycles it takes to draw on scanline
 const SCANLINE_CYCLES: i64 = 456;
@@ -18,9 +19,18 @@ impl LCD {
         }
     }
 
+    fn is_lcd_enabled(&self, mmu: &mut MMU) -> bool {
+        let test_bit = 0b1 << 1;
+        mmu.readb(LCD_CONTROL_REGISTER) & test_bit == test_bit
+    }
+
     pub fn update_graphics(&mut self, cycles: u8, mmu: &mut MMU) {
         // cycles: elasped cycles given by CPU
         // We access MMU memory directly because mmu::writeb() protects SCANLINE_REGISTER
+
+        if !self.is_lcd_enabled(mmu) {
+            return;
+        }
 
         self.scanlines_cycles -= cycles as i64;
 
